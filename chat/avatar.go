556a8d92ff530a7c1c027838c8cb419a79b0b2b5
BurtsE <burcev-egor@mail.ru>
2024-11-24 18:45:28 +0500

    avatar interface fix

--- chat/avatar.go
+++ chat/avatar.go
@@ -16,7 +16,7 @@ var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL")
 type Avatar interface {
 	// GetAvatarURL gets the avatar URL for the specified client,
 	// or returns an error if something goes wrong.
-	GetAvatarURL(user chatUser) (string, error)
+	GetAvatarURL(user ChatUser) (string, error)
 }
 
 type AuthAvatar struct{}
